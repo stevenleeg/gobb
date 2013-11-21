d8fa558af552af8c0c0a5d689bf7a4c358edbf0f
Steve Gattuso <stevenleeg@gmail.com>
2013-11-21 10:17:38 -0500

    Adding new postgres schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,8 +1,22 @@
-CREATE TABLE users(
-    id  INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE IF NOT EXISTS users (
+    id       SERIAL PRIMARY KEY,
     created  INTEGER,
-    username VARCHAR,
-    password VARCHAR,
-    salt     VARCHAR,
-    sid      VARCHAR
+    username VARCHAR(20),
+    password VARCHAR(75),
+    salt     VARCHAR(25)
+);
+
+CREATE TABLE IF NOT EXISTS boards (
+    id           SERIAL PRIMARY KEY,
+    title        VARCHAR(45),
+    description  VARCHAR(140)
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id         SERIAL PRIMARY KEY,
+    parent_id  INTEGER REFERENCES boards(id),
+    author_id  INTEGER REFERENCES users(id) NOT NULL,
+    title      VARCHAR(70),
+    content    TEXT,
+    created_on TIMESTAMP
 );
