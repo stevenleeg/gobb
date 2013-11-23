670b1375bb0c5677ee69e7c3c8735889625bd292
Steve Gattuso <stevenleeg@gmail.com>
2013-11-23 17:21:18 -0500

    Implementing post replying

--- controllers/thread.go
+++ controllers/thread.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
     "github.com/gorilla/mux"
+    "database/sql"
     "net/http"
     "strconv"
     "sirjtaa/utils"
@@ -16,6 +17,25 @@ func Thread(w http.ResponseWriter, r *http.Request) {
     post_id_str := mux.Vars(r)["post_id"]
     post_id, _ := strconv.Atoi(post_id_str)
     err, op, posts := models.GetThread(post_id)
+    op.GetAuthor()
+
+    if r.Method == "POST" {
+        db := models.GetDbSession()
+        title := r.FormValue("title")
+        content := r.FormValue("content")
+
+        current_user := utils.GetCurrentUser(r)
+        if current_user == nil {
+            http.NotFound(w, r)
+            return
+        }
+
+        post := models.NewPost(current_user, board, title, content)
+        post.ParentId = sql.NullInt64{ int64(post_id), true }
+        db.Insert(post)
+    }
+
+    err, op, posts = models.GetThread(post_id)
     op.GetAuthor()
 
     if err != nil {
