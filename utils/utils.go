86792b098f0083589d07525641360c29a947d6f6
James Anderson <james@jamesanderson.me>
2013-11-24 11:59:32 -0500

    site name config entry

--- utils/utils.go
+++ utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gorilla/sessions"
 	"log"
 	"net/http"
+    "sirjtaa/config"
 )
 
 var Store = sessions.NewCookieStore([]byte("83kjhsd98w3kjhwdfsdfw3"))
@@ -16,9 +17,12 @@ func RenderTemplate(
 	context map[string]interface{}) {
 
     current_user := GetCurrentUser(r)
+    site_name, _ := config.Config.GetString("gobb", "sitename")
+
 	send := map[string]interface{}{
         "current_user": current_user,
         "request": r,
+        "site_name": site_name,
 	}
 
 	// Merge the global template variables with the local context
