261057acce853fdfbe548f46ea62a561d71f0f7b
Steve Gattuso <stevenleeg@gmail.com>
2013-11-24 10:53:18 -0500

    Implementing board latest post

--- models/board.go
+++ models/board.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+    "fmt"
+)
+
 type Board struct {
     Id          int64  `db:"id"`
     Title       string `db:"title"`
@@ -16,4 +20,17 @@ func GetBoard(id int) (error, *Board) {
     }
 
     return nil, board
+}
+
+func (board *Board) GetLatestPost() *Post {
+    db := GetDbSession()
+    latest := &Post{}
+
+    err := db.SelectOne(latest, "SELECT * FROM posts WHERE board_id=$1 AND parent_id IS NULL ORDER BY created_on DESC LIMIT 1", board.Id)
+
+    if err != nil {
+        fmt.Printf("[error] Could not get latest post in board: (%s)\n", err.Error())
+    }
+
+    return latest
 }
