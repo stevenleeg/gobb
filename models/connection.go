6a8cd9e564a1c0d0e4382fa50d69064d686d05ea
Steve Gattuso <stevenleeg@gmail.com>
2013-12-04 13:41:23 -0500

    Maybe this will help

--- models/connection.go
+++ models/connection.go
@@ -25,6 +25,7 @@ func GetDbSession() *gorp.DbMap {
 		db_port = "5432"
 	}
 
+    fmt.Println("Opening connection!")
 	db, err := sql.Open("postgres",
 		"user="+db_username+
 			" password="+db_password+
