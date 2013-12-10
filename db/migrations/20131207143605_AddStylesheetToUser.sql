7f9c466c8307d84636da07e3d245c4c27581f570
Steve Gattuso <stevenleeg@gmail.com>
2013-12-09 19:05:41 -0500

    Fixes migration

--- db/migrations/20131207143605_AddStylesheetToUser.sql
+++ db/migrations/20131207143605_AddStylesheetToUser.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE users ADD COLUMN stylesheet_url varchar
+ALTER TABLE users ADD COLUMN stylesheet_url varchar;
 
 -- +goose Down
-ALTER TABLE users DROP COLUMN stylesheet_url
+ALTER TABLE users DROP COLUMN IF EXISTS stylesheet_url;
